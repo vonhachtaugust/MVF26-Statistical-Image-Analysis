10003236383172940c09b3b86f62d1bfc6538f46
SuperDavid <david_hansson_23@hotmail.com>
2016-04-24 17:40:24 +0200

    Added database

--- Camlab/Main.m
+++ Camlab/Main.m
@@ -1,32 +1,25 @@
 clc;
 clf;
+clear all;
 warning('off','all')
 
-% clear('cam')
-% cam = webcam(1);
-% cam.AvailableResolutions
-% cam.Resolution = '352x288';
-% preview(cam)
-% img = snapshot(cam);
-% image(img);
-% pause(.1)
-
-
 %%%%%% Parameters %%%%%%
-threshold = 140;
+threshold = 90;
 k = 1;
 sigma = .8;
 
 %%%%%% Read and paint image %%%%%%
 figure(1)
-img = imread('mtglol.png');
+img = imread('8.jpg');
+% img = imread('Realms-Uncharted.jpg');
+% img = imread('mtglol.png');
 imshow(img)
 
 %%%%%% Binarize and paint image %%%%%%
 tmpMatrix = img(:,:,1)/3 + img(:,:,2)/3 + img(:,:,3)/3;
 tmpMatrix = (tmpMatrix > threshold);
 figure(2)
-imshow(tmpMatrix);
+imshow(tmpMatrix); 
 
 %%%%%% Extract textbox (Assuming perfect align) %%%%%%
 vertpixs = floor(size(tmpMatrix(:,:,1),1)*0.88/8.8);
@@ -46,10 +39,23 @@ figure(4)
 imshow(blurred);
 
 %%%%%% Read and paint character %%%%%%
-charNumber = 2;
+charNumber = 1;
 texter = ocr(tmpMatrix);
 disp(texter.Text(charNumber));
 Bpos = texter.CharacterBoundingBoxes(charNumber,:);
-newTmpMatrix = tmpMatrix(Bpos(2):Bpos(2)+Bpos(4),Bpos(1):Bpos(1)+Bpos(3));
+newTmpMatrix = tmpMatrix(Bpos(2):Bpos(2)+Bpos(4)-1,Bpos(1):Bpos(1)+Bpos(3)-1);
 figure(5)
 imshow(newTmpMatrix)
+texter.Text
+%%%%%% Put all characters with glyph into database %%%%%%
+% alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+load('database.mat')
+for i = 1:size(texter.Text,2)-2
+    structName = texter.Text(i);
+    if (structName ~= ' ')% && ~(strcmp(structName, '\n'))
+        Bpos = texter.CharacterBoundingBoxes(i,:);
+        newTmpMatrix = tmpMatrix(Bpos(2):Bpos(2)+Bpos(4)-1,Bpos(1):Bpos(1)+Bpos(3)-1);
+        database.(structName).glyph = newTmpMatrix;
+    end
+end
+save('database.mat','database');
