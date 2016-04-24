2250915714a02d1b5ffc7fb56e71c6f537d9e8f7
SuperDavid <david_hansson_23@hotmail.com>
2016-04-24 14:33:22 +0200

    Added some foundational code and pictures

--- Camlab/Main.m
+++ Camlab/Main.m
@@ -1,29 +1,55 @@
 clc;
-clear('cam')
-threshold = 180;
+clf;
+warning('off','all')
 
-k = 2;
-sigma = 1.2;
-figure(1)
+% clear('cam')
 % cam = webcam(1);
 % cam.AvailableResolutions
 % cam.Resolution = '352x288';
 % preview(cam)
 % img = snapshot(cam);
-img = imread('mtglol.png');
 % image(img);
 % pause(.1)
-tmpMatrix = img(:,:,1) + img(:,:,2) + img(:,:,3);
-tmpMatrix = (tmpMatrix > threshold);
 
+
+%%%%%% Parameters %%%%%%
+threshold = 140;
+k = 1;
+sigma = .8;
+
+%%%%%% Read and paint image %%%%%%
+figure(1)
+img = imread('mtglol.png');
+imshow(img)
+
+%%%%%% Binarize and paint image %%%%%%
+tmpMatrix = img(:,:,1)/3 + img(:,:,2)/3 + img(:,:,3)/3;
+tmpMatrix = (tmpMatrix > threshold);
+figure(2)
 imshow(tmpMatrix);
 
-figure(2)
+%%%%%% Extract textbox (Assuming perfect align) %%%%%%
+vertpixs = floor(size(tmpMatrix(:,:,1),1)*0.88/8.8);
+vertindent = vertpixs * 0.54;
+hortpixs = floor(size(tmpMatrix(:,:,1),2)/6);
+hortindent = floor(size(tmpMatrix(:,:,1),2)*.5/6.3);
+tmpMatrix(:,1:hortindent,:) = [];
+tmpMatrix(:,end-hortindent:end,:) = [];
+tmpMatrix(vertpixs:end,:,:) = [];
+tmpMatrix(1: vertindent ,:,:) = [];
+figure(3)
+imshow(tmpMatrix);
 
+%%%%%% Apply Gaussian blur to textbox %%%%%%
+blurred = GaussianBlur(tmpMatrix,k,sigma);
+figure(4)
+imshow(blurred);
 
-
-% blurred = GaussianBlur(img,k,sigma);
-% figure(2)
-% image(blurred/255);
-
-% closePreview(cam)
+%%%%%% Read and paint character %%%%%%
+charNumber = 2;
+texter = ocr(tmpMatrix);
+disp(texter.Text(charNumber));
+Bpos = texter.CharacterBoundingBoxes(charNumber,:);
+newTmpMatrix = tmpMatrix(Bpos(2):Bpos(2)+Bpos(4),Bpos(1):Bpos(1)+Bpos(3));
+figure(5)
+imshow(newTmpMatrix)
