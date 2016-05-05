c6dd286f1a9b04cb1045113eb0415c84b812170f
August von Hacht <vonhachtaugust@Augusts-MacBook-Pro.local>
2016-05-05 18:36:46 +0200

    complete version

--- Camlab/binaryResample.m
+++ Camlab/binaryResample.m
@@ -5,8 +5,6 @@ function Ir = binaryResample(I, widthExtend, heightExtend);
 % New image sizes
 Nx_new = round(Nx*widthExtend);
 Ny_new = round(Ny*heightExtend);
-Ir = ones(Ny_new,Nx_new);
-
 
 % Width expand: Check each row (y), and store column (x) sequence.
 Ir_widthExpand = ones(Ny,Nx_new);
@@ -20,10 +18,13 @@ for i = 1:Ny % For each row:
         count = true;
         if I(i,j) == 0 % black pixel
             len(1) = j; % store start position
-            while count && j < Nx% count the sequence length
+            while count && j < Nx % count the sequence length
                 j = j + 1;
                 if I(i,j) ~= 0
                     len(2) = j; % store end position
+                    count = false;
+                elseif j == Nx
+                    len(2) = Nx;
                     count = false;
                 end
             end
@@ -43,26 +44,44 @@ for i = 1:Ny % For each row:
         end
     end
 end
-Ir = Ir_widthExpand;
+Iw = Ir_widthExpand;
 
+% Height expand: Check each column (x), and store row (x) sequence
+Ir_heightExpand = ones(Ny_new,Nx_new);
+len = zeros(1,2);
 
-
-% for i = 1:Ny_new % row
-%     for j = 1:Nx_new % col
-%         col = round(j/widthExtend); % Orig. col pos
-%         row = round(i/heightExtend); % Orig. row pos
-%
-%         if row >= 1 && row <= Ny_new && col >= 1 && col <= Nx_new
-%             Ir(i,j) = I(row,col);
-%         elseif row < 1
-%             Ir(i,j) = I(1,col);
-%         elseif row > Ny_new
-%             Ir(i,j) = I(Ny_new,col);
-%         elseif col < 1
-%             Ir(i,j) = I(row,1);
-%         elseif col > Nx_new
-%             Ir(i,j) = I(row,Nx_new);
-%         end
-%
-%     end
-% end
+for i = 1:Nx_new % For each column in extended I (Iw)
+    sequence = [];
+    
+    j = 1;
+    while j <= Ny % row loop
+        count = true;
+        if Iw(j,i) == 0 % black pixel
+            len(1) = j; % store start position
+            while count && j < Ny % count the sequence length
+                j = j + 1;
+                if Iw(j,i) ~= 0
+                    len(2) = j; % store end position
+                    count = false;
+                elseif j == Ny
+                    len(2) = Ny;
+                    count = false;
+                end
+            end
+        end
+        sequence = [sequence; len];
+        j = j + 1;
+    end
+    sequence = round(heightExtend.*sequence);
+    
+    for k = 1:size(sequence,1)
+        start = sequence(k,1);
+        End = sequence(k,2);
+        if abs(End-start) >= 1 && End > start
+            for m = start:End
+                Ir_heightExpand(m,i) = 0;
+            end
+        end
+    end
+end
+Ir = Ir_heightExpand;
