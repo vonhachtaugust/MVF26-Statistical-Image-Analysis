db3e4dd5b25227eb8d952a8c37cfbbb651cfe3cb
SuperDavid <david_hansson_23@hotmail.com>
2016-05-17 14:04:49 +0200

    New main file, fixed slight bug, cleaned up code, and added parameter initialization at begging

--- Camlab/Main2.m
+++ Camlab/Main2.m
@@ -13,31 +13,34 @@ NHOOD1 = [0,1,0; 1,0,1; 0,1,0]; %neumann neighbourhood
 NHOOD2 = [1,1,1; 1,0,1; 1,1,1]; %complete euclidean neighbourhood
 NHOOD3 = [0,0,0; 1,0,1; 0,0,0]; %horizontal neighbourhood
 
+%%%%%%%%%% PARAMETERS %%%%%%%%%%%
+
+maxAngleOfPicture = 10; % Maximum angle of picture in degrees
+binarizationThreshold = 80; %[0,255]
+dilates_and_erodes = 8; % Number of times to erode and dilate before hough transform
+houghLineThreshold = 0.5; % Threshold for how "strong" lines must be to be included
+maximumHoughLines = 18; % Maximum number of lines to be found by hough transformation
+searchResolution = 20; % Resolution when searching for borders of card, recommended max 1/50 of picture size, lower when large angles allowed for card
+
+%%%%%%%%%% END OF PARAMETERS %%%%%%%%%%%
+
 % Read image:
-I = imread('Images\dbImages\11.jpg');
-I = rgb2gray(I) > 80;
-I = imerode(imdilate(imerode(imdilate(I,NHOOD1),NHOOD1),NHOOD1),NHOOD1);
+I = imread('Images\dbImages\7.jpg');
+I = rgb2gray(I) > binarizationThreshold;
 figure(10), imshow(I);
-% Export\export_fig(
 I2 = I;
-
-
-dilates_and_erodes = 8;
 for i = 1 : dilates_and_erodes
     I2 = imdilate(I2,NHOOD2);
 end
 for j = 1 : dilates_and_erodes
     I2 = imerode(I2,NHOOD2);
 end
-
 figure(2), imshow(I2);
 
 % Hough transformation:----------------------------------------
 Ir_edge = edge(I2,'Canny');
 [H,T,R] = hough(Ir_edge, 'Theta', -10:0.2:10);
 [H2,T2,R2] = hough(Ir_edge, 'Theta', [(80:0.2:89.8),(-89.8:0.2:-80)]);
-
-% Plot of hough matrix and its peaks
 
 % Plot hough matrix:-------------------------------------------
 clf(1),figure(1),imshow(imadjust(mat2gray(H)),'XData',T,'YData',R,...
@@ -48,13 +51,11 @@ axis on, axis normal;
 colormap(hot);
 
 % Locate and plot peaks in hough matrix:------------------------
-P = houghpeaks(H,18,'threshold',ceil(0.5*max(H(:))));
-P2 = houghpeaks(H2,18,'threshold',ceil(0.5*max(H2(:))));
-% x = T(P(:,2));
-% y = R(P(:,1));
-% hold on, plot(x,y,'s','color','blue');
-
-% Find and plot vertical lines from hough transformation
+P = houghpeaks(H,maximumHoughLines,'threshold',ceil(houghLineThreshold*max(H(:))));
+P2 = houghpeaks(H2,maximumHoughLines,'threshold',ceil(houghLineThreshold*max(H2(:))));
+x = T(P(:,2));
+y = R(P(:,1));
+hold on, plot(x,y,'s','color','blue');
 
 % Locate lines based on peaks in hough matrix:------------------
 lines = houghlines(Ir_edge,T,R,P,'FillGap',5,'MinLength',7);
@@ -73,8 +74,6 @@ for i = 1:length(lines)
     sum = sum + lines(i).theta;
 end
 sum = sum/i;
-
-% Find and plot horizontal lines from hough transformation
 
 % Locate lines based on peaks in hough matrix:-----------------
 lines2 = houghlines(Ir_edge,T2,R2,P2,'FillGap',5,'MinLength',7);
@@ -109,9 +108,7 @@ NEWNEWIMAGE = ~BW2;
 figure(4), imshow(NEWNEWIMAGE)
 
 % Extend borders to frame:----------------------------------------
-searchResolution = 20;
 NEWNEWIMAGE = extendBorders(NEWNEWIMAGE,searchResolution);
-
 NEWNEWNEWIMAGE = imdilate(imerode(NEWNEWIMAGE,NHOOD2),NHOOD2);
 figure(5), imshow(NEWNEWNEWIMAGE);
 
@@ -119,20 +116,13 @@ figure(5), imshow(NEWNEWNEWIMAGE);
 % Extract Textbox:------------------------------------------------
 width = size(NEWNEWNEWIMAGE,2);
 height = size(NEWNEWNEWIMAGE,1);
-
 textbox = NEWNEWNEWIMAGE(0.06*height:0.11*height , 0.079*width:0.74*width);
-
 BW3 = imclearborder(~textbox);
 textbox = ~BW3;
-
-% textbox = imerode(imdilate(textbox,NHOOD2),NHOOD2);
-% textbox = imdilate(imerode(textbox,NHOOD2),NHOOD2);
-
 figure(6), imshow(textbox);
 
 % Extract each letter and insert into struct names "letters"
 clear letters
-
 middle = ceil(size(textbox,1) /2);
 counter = 0;
 i = 1;
