84f861722228668650df74e8ed9c44bbd75f328f
SuperDavid <david_hansson_23@hotmail.com>
2016-05-17 23:49:15 +0200

    Changed density function to consider letter size ratios, added function to calculate letter size ratio for database

---
+++ Camlab/databaseGlyphRatios.m
@@ -0,0 +1,10 @@
+load('database.mat');
+
+fields = char(fieldnames(database));
+for i = 1:size(fields,1)
+    currField = database.(fields(i));
+    currField.ratio = size(currField.glyph,1)/size(currField.glyph,2);
+    database.(fields(i)) = currField;
+end
+
+save('database.mat','database');
